1ecf13806b0214f4c4c0c1f9b3aabd69aa1b8487
Oleh Mamalyha <omamalyha@gmail.com>
2022-06-06 23:49:37 +0300

    done with responsive

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -20,21 +20,23 @@ const CurrentWeather = (props) => {
                     <div className="weather-container__main-temp--small">{props.data && props.toggleSystem(props.data.main.temp_min)}/{props.data && props.toggleSystem(props.data.main.temp_max)}</div>
                 </div>
                 <div className="weather-container__info">
-                    <div className="weather-container__info__icons">
+                    <div className="weather-container__info--itm">
                         <div className="info-icon clouds-icon">
                             <img src={require(`../icons/clouds.png`)} alt="clouds-icon" />
                         </div>
-                        <div className="info-icon wind-icon">
-                            <img src={require(`../icons/wind.png`)} alt="wind-icon" />
-                        </div>
+                        <div className="info-param">{props.data && props.data.clouds.all}%</div>
+                    </div>
+                    <div className="weather-container__info--itm info-humidity">
                         <div className="info-icon humidity-icon">
                             <img src={require(`../icons/humidity.png`)} alt="humidity-icon" />
                         </div>
+                        <div className="info-param">{props.data && props.data.main.humidity}%</div>
                     </div>
-                    <div className="weather-container__info__dem">
-                        <div>{props.data && props.data.clouds.all}%</div>
-                        <div>{props.data && props.toggleSystemDistance(props.data.wind.speed)}</div>
-                        <div>{props.data && props.data.main.humidity}%</div>
+                    <div className="weather-container__info--itm">
+                        <div className="info-icon wind-icon">
+                            <img src={require(`../icons/wind.png`)} alt="wind-icon" />
+                        </div>
+                        <div className="info-param">{props.data && props.toggleSystemDistance(props.data.wind.speed)}</div>
                     </div>
                 </div>
             </div>
